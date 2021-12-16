1ca1e965fcde6391e94e5c922278b45e457316f8
Haotian Gan <haotiangan@gmail.com>
2021-12-16 00:24:05 -0500

    Updates

--- app/__init__.py
+++ app/__init__.py
@@ -11,54 +11,52 @@ from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = 'Mango'
 
+
 # Utility function to check if there is a session
 def logged_in():
-    return session.get('username') is not None
+    return session.get('username') != None
 
 @app.route('/', methods=['GET', 'POST'])
 def landing():
-    # Check for session existance
-    #db_builder.dbseteup()
+    # Check for session existence
     if logged_in():
-        username = session['username']
-        # return render_template('index.html', username=username, search=filter)
-        user_id = db_builder.get_id_from_username(username)
-        return render_template('home.html', username=username, user_id=user_id)
+      username = session['username']
+      user_id = db_builder.get_id_from_username(username)
+      return render_template('home.html', username=username, user_id=user_id)
     else:
-        # If not logged in, show login page
-        return render_template('intro.html')
+      # If not logged in, show login page
+      return render_template('intro.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     method = request.method
-
     # Check for session existance
     if method == 'GET':
-        if logged_in():
-            return redirect(url_for('landing'))
-        else:
-            # If not logged in, show login page
-            return render_template('login.html', error=False)
+      if logged_in():
+        return redirect(url_for('landing'))
+      else:
+        # If not logged in, show login page
+        return render_template('login.html', error=False)
 
     if method == 'POST':
 
-        # Get information from request.form since it is submitted via post
-        username = request.form['username']
-        password = request.form['password']
-        error = db_builder.login(username, password)
+      # Get information from request.form since it is submitted via post
+      username = request.form['username']
+      password = request.form['password']
+      
 
-        if error:
-            # If incorrect, give feedback to the user
-            return render_template('login.html', error=error)
-        else:
-            # Store user info into a cookie
-            session['username'] = username
-            return redirect(url_for('landing'))
+      if db_builder.credentialsExist(username, password):
+          # Store user username as session cookie. This uniquely identifies the user
+          session['username'] = username
+          return redirect(url_for('landing'))
+      else:
+          # If credentials match, give feedback to the user
+          return render_template('login.html', error=error)
+          
 
 @app.route('/register', methods=['GET','POST'])
 def register():
     method = request.method
-
     # Check for session existence
     if method == "GET":
         if logged_in():
@@ -104,102 +102,6 @@ def logout():
     return redirect(url_for('landing'))
 
 
-####BLOG FUNCTIONS#####
-#######################
-
-@app.route('/blog/<int:user_id>', methods=['GET', 'POST'])
-def display_user_blog(user_id):
-    """Displays all entires of a user. Tracks if the user is the same one that is logged in"""
-
-    if not logged_in():
-        # If not logged in, redirect to the main page
-        return redirect(url_for('landing'))
-    
-    templateArgs = {
-        "entries" : db_builder.get_entries_of_user(user_id, 0, 50), #see get_entries_of_user in database.py
-        "username" : session.get('username'),#see get_username_from_id in database.py
-        "lookingAtOwnBlog": db_builder.get_id_from_username(session.get('username')) == user_id #session authentication for the user
-    }
-    return render_template(
-        'blog.html',
-        **templateArgs
-    )
-
-@app.route('/entry/<int:entry_id>', methods=['GET', 'POST'])
-def display_entry(entry_id):
-    """Displays an entry of another user's blog to the user"""
-    template_args = {}
-    try:
-        template_args = db_builder.get_entry(entry_id) #see get_entry in database.py
-        template_args["username"] = db_builder.get_username_from_id(template_args["user_id"])
-        template_args["original_author"] = session.get("user_id") == template_args["user_id"] #logic check if the user currently viewing a blog is viewing their own blog
-    except Exception:
-        return render_template("404.html")
-    return render_template(
-        'entry.html',
-        **template_args
-    )
-
-@app.route('/blog/newBlogEntry', methods=['GET', 'POST'])
-def create_new_entry():
-    """Allows the user to create a new blog entry"""
-    try:
-        if(session.get("user_id") == None):
-            return redirect("/") #if a non-logged in user gets here, redirect them to the login page
-        if(request.method == 'POST'):
-            user_id = session.get("user_id")
-            new_entry_text = request.form.get('entry_text')
-            new_title = request.form.get('title')
-            if(new_title != ''): #can't have blank title
-                db_builder.add_entry(new_title, new_entry_text, user_id) #see add_entry in database.py
-            else:
-                return redirect('/blog/newBlogEntry')
-
-            #This assumes that the entry_id of the one we just added to the database, is the user's most recent entry
-            assumed_entry_id = db_builder.getMostRecentEntry(user_id)["entry_id"]
-            return redirect(f"/entry/{assumed_entry_id}")
-        return render_template("newBlogEntry.html", user_id=session.get("user_id"))
-    except Exception:
-        return "Something went wrong on our end. Please try again later."
-
-@app.route('/entry/<int:entry_id>/edit', methods=['GET', 'POST'])
-def display_entry_edit(entry_id):
-    """Allows a user to edit their own entry"""
-    try:
-        if(session.get("user_id") == None):
-            return redirect("/")
-        if request.method == 'POST':
-            author_id = db_builder.get_entry(entry_id)["user_id"]
-            if(not author_id == session.get("user_id")):
-                return "Sorry, you can't modify this blog post if you aren't its author."
-            new_entry_text = request.form.get('entry_text')
-            new_title = request.form.get('title')
-            db_builder.edit_entry(entry_id, new_entry_text, new_title)
-            return redirect(f"/entry/{entry_id}")
-        if request.method == 'GET':
-            templateArgs = db_builder.get_entry(entry_id)
-            return render_template('entry_edit.html', **templateArgs) #displays the text boxes with current text and title of the post already displayed
-    except Exception:
-        return render_template("404.html")
-
-@app.route('/entry/delete', methods=['POST'])
-def delete():
-    """Removes a user's blog entry"""
-    try: 
-        if(session.get("user_id") == None):
-            return redirect("/")
-        entry_id = request.form["entry_id"]
-        author_id = db_builder.get_entry(entry_id)["user_id"] #see get_entry in database.py
-        user_id = session.get("user_id")
-        if(not author_id == user_id):
-            return "Sorry, you can't modify this blog post if you aren't its author."
-
-        db_builder.delete_entry(entry_id) #see delete_entry in database.py
-        return redirect(f"/blog/{user_id}")
-    except Exception:
-        return render_template("404.html")
-
 if __name__ == '__main__':
-    # db_builder.dbseteup()
     app.debug = True
     app.run()
--- app/db_builder.py
+++ app/db_builder.py
@@ -1,147 +1,116 @@
 '''
 Mango: Aryaman Goenka, Sadid Ethun, Haotian Gan
 Softdev
-P00: ArRESTed Development
+P01: ArRESTed Development
 2021-12-12
 '''
 
-import sqlite3   #enable control of an sqlite database
+import sqlite3   
 import random 
+import json
 
 DB_FILE="Mangos.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False)
 
-def dbseteup():
-    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+def setup():
+  c = db.cursor()
 
-    c.execute("DROP TABLE IF EXISTS Entries")
-    command = "CREATE TABLE Entries (ID INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT, Text TEXT, UserID INTEGER)"
-    c.execute(command) 
+  # Login Table:
+  command = "CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT, password TEXT)"
+  c.execute(command)   
 
-    c.execute("DROP TABLE IF EXISTS Users")
-    command = "CREATE TABLE Users (ID INTEGER PRIMARY KEY AUTOINCREMENT, Username TEXT, Password TEXT)"
-    c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
-    # run SQL statement
 
-    db.commit() #save changes
-    db.close()  #close database
+  # User Personal Info Table:
+    ## preferredCuisines, and intolerances are lists. Diet is a single string.
+      ### intolerances are things that the user cannot eat (i.e soy, egg, diary)
+      ### we'll use this data to filter search results
+    ## Supported cuisines are: https://spoonacular.com/food-api/docs#Cuisines
+    ## Supported intolerances are: https://spoonacular.com/food-api/docs#Intolerances
+    ## Supported diets are on the spoonacular api docs
+
+  command = "CREATE TABLE IF NOT EXISTS user_personal_info (user_id INTEGER NOT NULL, preferredCuisines TEXT DEFAULT '[]', diet TEXT DEFAULT '', intolerances TEXT DEFAULT '[]', displayName TEXT DEFAULT '', personalPageDescription TEXT DEFAULT '')"
+  c.execute(command) 
+
+  # User Favorite Resturants:
+    # To do
+
+  # User Favorite Recipes: 
+    # Recipes can be organized into their own folders, just like songs on spotify. 
+  command = "CREATE TABLE IF NOT EXISTS user_favorite_recipe (user_id INTEGER NOT NULL, spoonacular_recipe_id INTEGER, spoonacularRecipeJSON TEXT, folder TEXT)"
+  c.execute(command) 
+
+  # User Recipe Folders:
+    # This is used to keep track of how many folders a user has
+  command = "CREATE TABLE IF NOT EXISTS user_folder (user_id, folder TEXT)"
+  c.execute(command) 
+
+  # User Favorite Posts:
+  command = "CREATE TABLE IF NOT EXISTS user_favorite_post (user_id INTEGER NOT NULL, post_id INTEGER)"
+  c.execute(command) 
+
+  # User Posts:
+  command = "CREATE TABLE IF NOT EXISTS user_post (user_id INTEGER NOT NULL, post_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, recipe_id INTEGER, image_link TEXT, post_description TEXT)"
+  c.execute(command) 
+
+  # Post Comments:
+  command = "CREATE TABLE IF NOT EXISTS user_comment (comment_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL, post_id INTEGER NOT NULL, comment TEXT)"
+  c.execute(command) 
+
+  db.commit() 
+
+setup()
+
+# Authorization, username, and user_id functions
+
+def credentialsExist(username, password):
+  "If the username and password combination matches a database entry, this returns True. Otherwise, it returns False"
+  c = db.cursor()
+  c.execute("SELECT username FROM user WHERE username=? AND password=?", [username, password])
+  result = c.fetchone()
+  return bool(result)
+
+def get_id_from_username(username):
+  """returns the user id given the username"""
+  c = db.cursor()
+  c.execute("SELECT user_id from user where username == ?", [username])
+  row = c.fetchone()
+  if row:
+    return row[0]
+  raise Exception("No user_id for that username")
+      
+def get_username_from_id(user_id):
+  """returns the username given the user_id"""
+  c = db.cursor()
+  c.execute(f'SELECT username from user where user_id == ?', [user_id])
+  result = c.fetchone()
+  if result:
+    return result[0]
+  raise Exception("No username for that user_id")
 
 # Adds to the user database if the username is availible
 # Returns an error message to display if there was an issue or an empty string otherwise
 def signup(username, password):
     c = db.cursor()
-
-
-    c.execute("""SELECT Username FROM Users WHERE Username=?""",[username])
+    c.execute("SELECT username FROM user WHERE username=?", [username])
     result = c.fetchone()
 
     if result:
         return "Error: Username already exists"
 
     else:
-        c.execute('INSERT INTO Users VALUES (null, ?, ?)', (username, password))
-
+        c.execute('INSERT INTO user (username, password) VALUES (?, ?)', [username, password])
         db.commit()
-        db.close()
+        c.execute('INSERT INTO user_personal_info (user_id, preferredCuisines, diet, intolerances, displayName, personalPageDescription) VALUES (?, ?, ?, ?, ?, ?)', 
+                  [get_id_from_username(username), json.dumps([]), "", json.dumps([]), "", ""])
+        db.commit()
 
         # Uses empty quotes since it will return false when checked as a boolean
-        return  ""
-
-# Tries to check if the username and password are valid
-def login(username, password):
-    c = db.cursor()
-
-    c.execute("""SELECT Username FROM Users WHERE Username=? AND Password=?""",[username, password])
-    result = c.fetchone()
-
-    if result:
-        ##access this specifc user data
-        return False
-
-    else:
-        return True
-
-def add_entry(title, entry_text, user_id):
-    """Adds an entry into the entries table, stores the text, title, and the user id of the associated account"""
-    c = db.cursor()
-    c.execute(f'INSERT INTO entries (title, entry_text, user_id) VALUES (?, ?, ?)', (title, entry_text, user_id))
-    db.commit() 
-
-def edit_entry(entry_id, entry_text, title):
-    """Updates the text and title of an entry, entry id is not altered"""
-    c = db.cursor()
-    c.execute(f'UPDATE entries SET entry_text = ?, title = ? where entry_id == ?', (entry_text, title, entry_id))
-    db.commit()
-
-def delete_entry(entry_id):
-    """Removes an entry from the table based on the entry id"""
-    c = db.cursor()
-    c.execute(f'delete from entries where entry_id == ?', (entry_id))
-    db.commit()
-
-def get_entry(entry_id):
-    """Returns a list based on the entry id with values of the id, text, title, and assosciated user id"""
-    c = db.cursor()
-    result = list(c.execute(f'select entry_id, entry_text, title, user_id from entries where entry_id == ?', (entry_id, )))
-    if(len(result) == 0): #if there is no entry with the id 
-        return None
-    return [{
-        "entry_id": entry_id,
-        "entry_text": entry_text,
-        "title": title,
-        "user_id": user_id
-    } for (entry_id, entry_text, title, user_id) in result][0] #returning only the selected entry
-
-## (edited)
-def get_entries_of_user(user_id, offset, limit):
-    """Returns a list of lists with user entries from the offset to the limit"""
-    c = db.cursor()
-    result = list(c.execute(f'SELECT ID, Text, Title, UserID FROM Entries WHERE UserID == ? order by ID limit ? offset ? ', (user_id, limit, offset)))
-    return [{
-        "entry_id": entry_id,
-        "entry_text": entry_text,
-        "title": title,
-        "user_id": user_id
-    } for (entry_id, entry_text, title, user_id) in result] #all the entries of a user
+        return ""
 
 
-## (edited)
-def get_id_from_username(username):
-    """returns the user id given the username"""
-    c = db.cursor()
-    c.execute("SELECT ID from Users where Username == ?", [username])
-    row = c.fetchone()
-    if row is not None:
-        result = row[0]
-    return result
 
-def get_username_from_id(user_id):
-    """returns the username given the user_id"""
-    c = db.cursor()
-    result = list(c.execute(f'SELECT Username from Users where ID == ?', (user_id, )))[0][0]
-    return result
 
-def getMostRecentEntry(user_id):
-    """Returns the users most recent entry by ordering all of their entries in id order, with the entry of the largest id at the top"""
-    c = db.cursor()
-    result = list(c.execute(f'select entry_id, entry_text, title, user_id from entries where user_id == ? order by entry_id DESC limit 1', (user_id, )))
-    return [{
-        "entry_id": entry_id,
-        "entry_text": entry_text,
-        "title": title,
-        "user_id": user_id
-    } for (entry_id, entry_text, title, user_id) in result][0] #returning first entry in reordered list
+  
+  
 
-def get_random_users():
-    """Returns either 10 random users or all users to display as recommended blogs on the homepage"""
-    c = db.cursor()
-    rows = list(c.execute('SELECT COUNT(*) FROM users'))[0][0] #length of users table
-    population_count = 10 if rows >= 10 else rows
-    user_ids = random.sample(range(1,rows+1), population_count) #takes 10 distinct random users
-    usernames = [get_username_from_id(user_id) for user_id in user_ids]
-    return [
-        {
-            'username': username,
-            'user_id': user_id
-        } 
-    for (username, user_id) in zip(usernames, user_ids)]
+
